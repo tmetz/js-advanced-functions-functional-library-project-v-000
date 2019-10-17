5335c3fd029905d1c36b4d37e12dff626ea7b373
Tammy Metz <tammy@tammymetz.com>
2019-10-17 14:28:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,12 @@ const fi = (function() {
       return collection;
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection);
+      for (var i = 0; i < newCollection.length; i++) {
+        callback(newCollection[i]);
+      }
+      return newCollection;
     },
 
     reduce: function() {
