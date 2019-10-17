a57430cbefd4ec75be364e3f653cc5ae73755f78
Tammy Metz <tammy@tammymetz.com>
2019-10-17 14:19:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      if (collection instanceof Array) {
+        const newCollection = collection.slice();
+      }
+      else {
+        const newCollection = Object.values(collection);
+      }
+      
+      for (var i = 0; i < collection.length; i++) {
+        callback(newCollection[i]);
+      }
+      return collection;
     },
 
     map: function() {
